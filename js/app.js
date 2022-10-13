e09188ee6dc0746483fe11346ab8326907ac10be
micarsan <miguelangel.s@260606@cesurformacion.com>
2022-10-13 08:21:19 +0200

    Terminado: Eliminar el párrafo original con la instrucción remove.

--- js/app.js
+++ js/app.js
@@ -13,18 +13,18 @@ function lista_add_links() {
     let cantidad_elementos = ol.getElementsByTagName('li').length + 1;
 
     let nuevos_elementos = 3; // cantidad de elementos a añadir
-    let contenido = ''; // variable con el contenido a añadir
+    let content = ''; // variable con el contenido a añadir
 
     // Añadimos los elementos
     for ( let i=cantidad_elementos ; i < cantidad_elementos+nuevos_elementos ; i++ ) {
         log += '\n-Añadiendo a la lista: object ' + i;
-        contenido += '<li>object ' + i + '</li>';
+        content += '<li>object ' + i + '</li>';
     }
 
     console.log(log);
 
     // Añadimos el contenido
-    ol.innerHTML += contenido;
+    ol.innerHTML += content;
 }
 
 
@@ -78,10 +78,10 @@ function table_create_5x5() {
 function p_iterate_change() {
     console.log('- Enunciado: Iterar los párrafos y modificar el contenido con la propiedad textContent.');
 
-    let parrafos = document.querySelectorAll('p');
+    let paragraphs = document.querySelectorAll('p');
 
-    for( let contenido of parrafos ) {
-        contenido.textContent = '¡¡Párrafo modificado!! -- ' + contenido.textContent;
+    for( let paragraph of paragraphs ) {
+        paragraph.textContent = '¡¡Párrafo modificado!! -- ' + paragraph.textContent;
     }
 }
 
@@ -130,9 +130,9 @@ function css_change() {
 function p_new_append() {
     console.log('- Enunciado: Crear un nuevo elemento párrafo y añadirlo al div correspondiente (pista: appendChild).');
 
-    let parrafo = document.createElement('p');
-    parrafo.textContent = 'Nuevo párrafo creado.';
-    document.body.appendChild( parrafo );
+    let paragraph = document.createElement('p');
+    paragraph.textContent = 'Nuevo párrafo creado.';
+    document.body.appendChild( paragraph );
 }
 
 
@@ -140,21 +140,88 @@ function p_new_append() {
 function botton_new_color_blind() {
     console.log('- Enunciado: Crear un nuevo elemento botón con un evento que nos permita al clicar, cambiar el color de todas las letras del body alternando entre azul y rojo.');
 
-    let boton = document.createElement('button');
-    boton.setAttribute('onclick', '(document.body.style.color=="red") ? document.body.style.color="blue" : document.body.style.color="red"');
-    boton.textContent = 'Cambiar color de la letra';
-    document.body.appendChild( boton );
+    let button_element = document.createElement('button');
+    button_element.setAttribute('onclick', '(document.body.style.color=="red") ? document.body.style.color="blue" : document.body.style.color="red"');
+    button_element.textContent = 'Cambiar color de la letra';
+    document.body.appendChild( button_element );
 }
 
 
 
 function div_p_duplicate() {
-    console.log('- Enunciado: Clonar el div contenedor de los párrafos con sus contenidos y añadirlo al body.');
+    log = '- Enunciado: Clonar el div contenedor de los párrafos con sus contenidos y añadirlo al body.';
+
+    // Cogemos todos los párrafos contenidos en div
+    let paragraphs = document.querySelectorAll('div>p');
+
+    console.log( log + '\n- Hay '+ paragraphs.length + ' párrafos. Clonando y añadiendo al final del contenido' );
+    
+    let last_div; // para guardar el div anterior y comprobar si el siguiente p corresponde al mismo div
+    for( let paragraph of paragraphs ) {
+
+        let div = paragraph.parentElement; // cogemos el elemento padre (el div)
+        if( div !== last_div ) {
+            // Clonamos y añadimos al body
+            document.body.appendChild( div.cloneNode(true) );
+        }
+        last_div = div; 
+    }
+
 }
 
 
 
 function div_p_delete_origin() {
-    console.log('- Enunciado: Eliminar el párrafo original con la instrucción remove.');
+    log = '- Enunciado: Eliminar el párrafo original con la instrucción remove.';
+
+    // Cogemos todos los párrafos contenidos en div
+    let paragraphs = document.querySelectorAll('div>p');
+
+    // Los recorremos y cogemos los padres (div) sin duplicados
+    let last_div; // para guardar el div anterior y comprobar si el siguiente p corresponde al mismo div
+    let divs = []; // array para guardar los div
+    for( let paragraph of paragraphs ) {
+
+        let div = paragraph.parentElement; // cogemos el elemento padre (el div)
+        if( div !== last_div ) {
+            // Añadimos al array divs
+            divs.push( div );
+        }
+        last_div = div; 
+    }
+
+    log += '\n- Hay '+ divs.length + ' contenedores de párrafos. Buscamos duplicados.';
+
+    let deleted = []; // Array para guardar los índices de los elementos eliminados
+
+    // Recorremos el array comparando cada elemento con los siguientes para encontrar duplicados y eliminar todos los "originales"
+    for( let i=0 ; i<divs.length ; i++ ) {
+        for( let j=0 ; j<divs.length ; j++ ) {
+           
+            // Saltamos el mismo elemento
+            if( i !== j ) {
+
+                // Comprobamos si ese elemento ya se eliminó
+                let is_deleted = false;
+                for( let del of deleted ) {
+                    if( del === j ) {
+                        is_deleted = true;
+                        break;
+                    }
+                }
+                
+                if( !is_deleted && divs[i].innerHTML === divs[j].innerHTML ) {
+                    log += '\n- Encontrado duplicado entre la posición '+i+' y '+j+'. Eliminando la posición '+i;
+                    deleted.push(i);
+                    divs[i].remove();
+                    break;
+                }
+           }
+        }
+    }
+
+    log += '\n- Se han eliminado '+ deleted.length + ' contenedores de párrafos.';
+    console.log( log );
+
 }
 
